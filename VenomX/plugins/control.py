96ad886458a201a1bbd56c2c644b2f5efa708732
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-09-08 18:30:36 +0530

    Update control.py

--- VenomX/plugins/control.py
+++ VenomX/plugins/control.py
@@ -3,6 +3,8 @@ from VenomX import add_to_queue, get_from_queue, get_media_stream
 from VenomX import clear_queue, is_queue_empty, task_done
 from pyrogram import filters
 from pytgcalls.exceptions import GroupCallNotFound
+from pytgcalls.types import StreamAudioEnded, StreamVideoEnded
+from pytgcalls.types.stream import StreamType
 
 
 @app.on_message(cdz(["pse", "pause", "vpse", "vpause"]) & ~filters.private)
@@ -11,13 +13,13 @@ async def pause_stream(client, message):
         return
     chat_id = message.chat.id
     try:
-        a = await call.get_call(chat_id)
-        if a.status == "playing":
+        call_info = await call.get_call(chat_id)
+        if call_info.is_playing:
             await call.pause_stream(chat_id)
             return await eor(message, "**Stream Paused!**")
-        elif a.status == "paused":
+        elif call_info.is_paused:
             return await eor(message, "**Already Paused!**")
-        elif a.status == "not_playing":
+        else:
             return await eor(message, "**Nothing Streaming!**")
     except GroupCallNotFound:
         return await eor(message, "**I am Not in VC!**")
@@ -29,13 +31,13 @@ async def resume_stream(client, message):
         return
     chat_id = message.chat.id
     try:
-        a = await call.get_call(chat_id)
-        if a.status == "paused":
+        call_info = await call.get_call(chat_id)
+        if call_info.is_paused:
             await call.resume_stream(chat_id)
             return await eor(message, "**Stream Resumed!**")
-        elif a.status == "playing":
+        elif call_info.is_playing:
             return await eor(message, "**Already Playing!**")
-        elif a.status == "not_playing":
+        else:
             return await eor(message, "**Nothing Streaming!**")
     except GroupCallNotFound:
         return await eor(message, "**I am Not in VC!**")
@@ -47,32 +49,30 @@ async def skip_stream(client, message):
         return
     chat_id = message.chat.id
     try:
-        a = await call.get_call(chat_id)
-        if (a.status == "playing"
-            or a.status == "paused"
-        ):
+        call_info = await call.get_call(chat_id)
+        if call_info.is_playing or call_info.is_paused:
             await task_done(chat_id)
             queue_empty = await is_queue_empty(chat_id)
-            if queue_empty :
+            if queue_empty:
                 await call.leave_group_call(chat_id)
                 return await eor(
                     message,
                     "**🚫 Hey, Queue is Empty,\nSo Leaving VC❗...**"
                 )
-            check = await get_from_queue(chat_id)
-            media = check["media"]
-            type = check["type"]
-            stream = await get_media_stream(media, type)
+            next_in_queue = await get_from_queue(chat_id)
+            media = next_in_queue["media"]
+            media_type = next_in_queue["type"]
+            stream = await get_media_stream(media, media_type)
             await call.change_stream(chat_id, stream)
             return await eor(
                 message,
                 "Now Streaming ..."
             )
-        elif a.status == "not_playing":
+        else:
             return await eor(message, "**Nothing Playing!**")
     except GroupCallNotFound:
         return await eor(message, "**I am Not in VC!**")
-    
+
 
 @app.on_message(cdz(["stp", "stop", "end", "vend"]) & ~filters.private)
 async def cease_stream(client, message):
@@ -80,14 +80,10 @@ async def cease_stream(client, message):
         return
     chat_id = message.chat.id
     try:
-        a = await call.get_call(chat_id)
-        if (a.status == "not_playing"
-            or a.status == "playing"
-            or a.status == "paused"
-        ):
+        call_info = await call.get_call(chat_id)
+        if call_info.is_playing or call_info.is_paused:
             await clear_queue(chat_id)
             await call.leave_group_call(chat_id)
             return await eor(message, "**Stream Ended!**")
     except GroupCallNotFound:
         return await eor(message, "**I am Not in VC!**")
-    
